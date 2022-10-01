dec60b77308bc50217bcdef81631f6db0950004a
ujin <ujinael@gmail.com>
2022-10-02 02:01:59 +0300

    readmi

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import VHomePage from '@/pages/home/VHomePage.vue'
 import VSalesPage from '@/pages/sales/VSalesPage.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
